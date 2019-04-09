0a0f96d1992c2894d36bec4ab99887b91fda7837
JinxCY93 <vincentjinx@hotmail.com>
2019-04-10 00:09:54 +0800

    Cart div incomplete. price total incomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,42 @@
-import { Component } from '@angular/core'
-import { Timeouts } from 'selenium-webdriver';
-import { timeout } from 'q';
+import { Component, SystemJsNgModuleLoader } from '@angular/core'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 
 })
+
 export class AppComponent {
   times = 0;
   visible = false;
   price1 = 0;
   price2 = 0;
   itemtotalprice = 0;
+  id: string;
+
+  systems: object = [
+    {
+      system: 'Dell XPS 15',
+      processor: 'i7-7960K',
+      ram: '16GB',
+      hdd: '256GB M2 SSD + 1TB HDD 5400rpm',
+      display: '15.6 Adobe RGB Display',
+      imagepath: 'https://i.dell.com/is/image/DellContent//content/dam/global-site-design/product_images/dell_client_products/notebooks/xps_notebooks/xps_15_9570/spi/ng/notebook-xps-15-9570-bestof-500-ng.psd?fmt=png-alpha',
+      price: 5500,
+      quantity: 0,
+    },
+    {
+      system: 'Dell Inspiron 5000',
+      processor: 'i5-7960K',
+      ram: '8GB',
+      hdd: '128GB M2 SSD + 500GB HDD 5400rpm',
+      display: '15.6 Anti-Glare Display',
+      imagepath: 'https://i.dell.com/is/image/DellContent//content/dam/global-site-design/product_images/dell_client_products/notebooks/inspiron_notebooks/15_5584/global_spi/spi_inspiron_15_5584_1sp_silver/ng/notebook-inspiron-15-5584-1sp-silver-bestof-500-ng.psd?fmt=jpg',
+      price: 3500,
+      quantity: 0,
+    }
+  ]
 
   // clearcart testing purpose
   testingitem: object = {
@@ -22,14 +46,15 @@ export class AppComponent {
   carttesting = Object.keys(this.testingitem)
   // clearcart testing purpose
 
-  quantitycountadd() {
-    this.times++
+  quantitycountadd(systems) {
+    systems.quantity++
   }
-  quantitycountminus() {
-    if (this.times <= 0) {
+
+  quantitycountminus(systems) {
+    if (systems.quantity <= 0) {
       return 0
     } else {
-      this.times--
+      systems.quantity--
     }
   }
 
